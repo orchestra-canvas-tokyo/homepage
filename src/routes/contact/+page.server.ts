12ca4312dddd9d854b45d3dc354aedda406930ce
horn553 <36446753+horn553@users.noreply.github.com>
2024-11-03 19:47:00 +0900

    引き続きデバッグ

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,5 +1,5 @@
 import type { Actions, ServerLoad } from '@sveltejs/kit';
-// import { validate } from './validator';
+import { validate } from './validator';
 // import { log } from './logger';
 // import { verifyCaptcha } from './capthaVerfier';
 // import { sendEmail } from './emailSender';
@@ -20,9 +20,9 @@ export const load: ServerLoad = async ({ locals }) => {
 
 export const actions = {
 	// default: async ({ request, locals, platform }) => {
-	default: async () => {
+	default: async ({ locals, request }) => {
 		try {
-			// const { session } = locals;
+			const { session } = locals;
 
 			// const RECAPTCHA_SECRET = import.meta.env.VITE_RECAPTCHA_SECRET;
 			// const RESEND_API_KEY = import.meta.env.VITE_RESEND_API_KEY;
@@ -41,19 +41,19 @@ export const actions = {
 			// 	};
 			// }
 
-			// const rawRequest: Record<string, FormDataEntryValue> = {};
-			// (await request.formData()).forEach((value, key) => {
-			// 	rawRequest[key] = value;
-			// });
+			const rawRequest: Record<string, FormDataEntryValue> = {};
+			(await request.formData()).forEach((value, key) => {
+				rawRequest[key] = value;
+			});
 
-			// // バリデーション
-			// const validationResult = validate(rawRequest);
-			// if (!validationResult.success) return { success: false, message: 'Invalid request body' };
-			// const validatedRequest = validationResult.data;
+			// バリデーション
+			const validationResult = validate(rawRequest);
+			if (!validationResult.success) return { success: false, message: 'Invalid request body' };
+			const validatedRequest = validationResult.data;
 
-			// // csrfトークンを検証
-			// if (session.data.csrfToken !== validatedRequest.csrfToken)
-			// 	return { success: false, message: 'Invalid csrf token' };
+			// csrfトークンを検証
+			if (session.data.csrfToken !== validatedRequest.csrfToken)
+				return { success: false, message: 'Invalid csrf token' };
 
 			// // reCAPTCHAトークンを検証
 			// const captchaResult = await verifyCaptcha(validatedRequest.reCaptchaToken, RECAPTCHA_SECRET);
