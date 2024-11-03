ac9d6db4f2cde772bbbf23923bdb4efe9652b199
horn553 <36446753+horn553@users.noreply.github.com>
2024-11-03 19:32:56 +0900

    デバッグ

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,8 +1,8 @@
 import type { Actions, ServerLoad } from '@sveltejs/kit';
-import { validate } from './validator';
-import { log } from './logger';
-import { verifyCaptcha } from './capthaVerfier';
-import { sendEmail } from './emailSender';
+// import { validate } from './validator';
+// import { log } from './logger';
+// import { verifyCaptcha } from './capthaVerfier';
+// import { sendEmail } from './emailSender';
 
 export const load: ServerLoad = async ({ locals }) => {
 	const { session } = locals;
@@ -19,54 +19,55 @@ export const load: ServerLoad = async ({ locals }) => {
 };
 
 export const actions = {
-	default: async ({ request, locals, platform }) => {
+	// default: async ({ request, locals, platform }) => {
+	default: async () => {
 		try {
-			const { session } = locals;
+			// const { session } = locals;
 
-			const RECAPTCHA_SECRET = import.meta.env.VITE_RECAPTCHA_SECRET;
-			const RESEND_API_KEY = import.meta.env.VITE_RESEND_API_KEY;
+			// const RECAPTCHA_SECRET = import.meta.env.VITE_RECAPTCHA_SECRET;
+			// const RESEND_API_KEY = import.meta.env.VITE_RESEND_API_KEY;
 
-			if (!RECAPTCHA_SECRET || !RESEND_API_KEY) {
-				console.log({ RECAPTCHA_SECRET, RESEND_API_KEY });
-				return {
-					success: false,
-					message: 'Invalid system environment',
-					details: {
-						RECAPTCHA_SECRET,
-						RESEND_API_KEY,
-						'import.meta.env': import.meta.env,
-						'platform.env': platform?.env
-					}
-				};
-			}
+			// if (!RECAPTCHA_SECRET || !RESEND_API_KEY) {
+			// 	console.log({ RECAPTCHA_SECRET, RESEND_API_KEY });
+			// 	return {
+			// 		success: false,
+			// 		message: 'Invalid system environment',
+			// 		details: {
+			// 			RECAPTCHA_SECRET,
+			// 			RESEND_API_KEY,
+			// 			'import.meta.env': import.meta.env,
+			// 			'platform.env': platform?.env
+			// 		}
+			// 	};
+			// }
 
-			const rawRequest: Record<string, FormDataEntryValue> = {};
-			(await request.formData()).forEach((value, key) => {
-				rawRequest[key] = value;
-			});
+			// const rawRequest: Record<string, FormDataEntryValue> = {};
+			// (await request.formData()).forEach((value, key) => {
+			// 	rawRequest[key] = value;
+			// });
 
-			// バリデーション
-			const validationResult = validate(rawRequest);
-			if (!validationResult.success) return { success: false, message: 'Invalid request body' };
-			const validatedRequest = validationResult.data;
+			// // バリデーション
+			// const validationResult = validate(rawRequest);
+			// if (!validationResult.success) return { success: false, message: 'Invalid request body' };
+			// const validatedRequest = validationResult.data;
 
-			// csrfトークンを検証
-			if (session.data.csrfToken !== validatedRequest.csrfToken)
-				return { success: false, message: 'Invalid csrf token' };
+			// // csrfトークンを検証
+			// if (session.data.csrfToken !== validatedRequest.csrfToken)
+			// 	return { success: false, message: 'Invalid csrf token' };
 
-			// reCAPTCHAトークンを検証
-			const captchaResult = await verifyCaptcha(validatedRequest.reCaptchaToken, RECAPTCHA_SECRET);
-			if (!captchaResult) return { success: false, message: 'Invalid reCAPTCHA token' };
+			// // reCAPTCHAトークンを検証
+			// const captchaResult = await verifyCaptcha(validatedRequest.reCaptchaToken, RECAPTCHA_SECRET);
+			// if (!captchaResult) return { success: false, message: 'Invalid reCAPTCHA token' };
 
-			// データベースにログ
+			// // データベースにログ
 			// const loggingResult = await log(platform?.env.DB, {
 			// 	sentAt: new Date().toISOString(),
 			// 	...validatedRequest
 			// });
 			// if (!loggingResult.success) return { success: false, message: 'Failed to log' };
 
-			// メール送信
-			sendEmail(validatedRequest, RESEND_API_KEY);
+			// // メール送信
+			// sendEmail(validatedRequest, RESEND_API_KEY);
 
 			return { success: true };
 		} catch (error) {
