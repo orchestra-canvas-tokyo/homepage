b0260193ed26f18821b7d16ba715f9299af8814d
horn553 <36446753+horn553@users.noreply.github.com>
2024-11-04 18:56:45 +0900

    テスト関数を削除

--- src/routes/test/+server.ts
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = (({ platform }) => {
-	return new Response(JSON.stringify({ platform: Object.keys(Object(platform?.env)) }));
-}) satisfies RequestHandler;
