ba87dc2cd1fa85dd05d7c38c027138cfc5e7f870
horn553 <36446753+horn553@users.noreply.github.com>
2024-12-17 21:38:11 +0900

    SessionにきちんとSecretを導入

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 import { sveltekitSessionHandle } from 'svelte-kit-sessions';
 import KvStore from 'svelte-kit-connect-cloudflare-kv';
 import type { Store } from 'svelte-kit-sessions';
+import { SESSION_SECRET } from '$env/static/private';
 
 declare module 'svelte-kit-sessions' {
 	interface SessionData {
@@ -16,7 +17,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 		// https://kit.svelte.dev/docs/adapter-cloudflare#bindings
 		const store = new KvStore({ client: event.platform.env.KV }) as Store;
 		sessionHandle = sveltekitSessionHandle({
-			secret: 'secret',
+			secret: SESSION_SECRET,
 			store
 		});
 	}
