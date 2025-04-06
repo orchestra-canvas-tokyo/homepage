367b6feea262073a1a1fe13e0f0aa54b6829922d
horn553 <36446753+horn553@users.noreply.github.com>
2025-04-07 06:08:41 +0900

    eslintの循環参照エラー回避のため、plugin:svelte/recommendedをコメントアウト

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,7 +35,7 @@ export default [
 	...compat.extends(
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
+		// 'plugin:svelte/recommended',
 		'prettier'
 	),
 	{
