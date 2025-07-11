01404886bdb1ae66895a2598916640eaa7fca435
Kosuke Kitaura <36446753+horn553@users.noreply.github.com>
2025-07-12 06:50:13 +0900

    15回プログラム公開、チケット販売

--- src/lib/concerts/regular/regular15.ts
+++ src/lib/concerts/regular/regular15.ts
@@ -18,15 +18,28 @@ export const concert: Concert = {
 	conductor: {
 		name: '石﨑真弥奈'
 	},
-	// programs: [
-	// 	{
-	// 		name: '●',
-	// 		composer: '●'
-	// 	}
-	// ],
-	// ticket: {
-	// 	url: '●'
-	// },
+	soloist: {
+		title: 'ヴァイオリン',
+		name: '北川千紗',
+	},
+	programs: [
+		{
+	 		name: 'チャイコフスキー',
+	 		composer: '歌劇『エフゲニー・オネーギン』より ワルツ'
+	 	},
+		{
+	 		name: 'チャイコフスキー',
+	 		composer: 'ヴァイオリン協奏曲 ニ長調 作品35'
+	 	},
+		{
+	 		name: 'ストラヴィンスキー',
+	 		composer: 'バレエ音楽『火の鳥』組曲（1945年版）'
+	 	}
+	],
+	ticket: {
+		description: '全席指定 1,000円',
+	 	url: 'https://teket.jp/1776/52506'
+	},
 	showLinkToProgramNote: false
 	// cspell: disable-next-line
 	// youtubePlaylistId: '●'
