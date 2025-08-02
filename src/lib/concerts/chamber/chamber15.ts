235db495cf3432381b8c8cc6606c63a5ae437628
Ueda-Yuto <ikimono.gnsowm.3@gmail.com>
2025-08-03 01:20:30 +0900

    16回定期,17回定期と15回室内楽を追加

---
+++ src/lib/concerts/chamber/chamber15.ts
@@ -0,0 +1,28 @@
+import { getConcertShortName } from '../generateContentsToDisplay';
+import type { Concert } from '../types';
+// import flyer from './images/flyers/chamber-15.webp';
+
+const type = 'chamber';
+const number = 15;
+export const concert: Concert = {
+	type: type,
+	number: number,
+	slug: `${type}-${number}`,
+	title: `第${number}回${getConcertShortName(type)}演奏会`,
+	// flyer: flyer,
+	dateTime: { date: '2026-1-11', time: '昼公演' },
+	place: {
+		name: '横浜市戸塚区民文化センター さくらホール',
+		url: 'https://totsuka.hall-info.jp/access/'
+	},
+	// programs: [
+	// 	{ composer: '●', title: '●' },
+	// ],
+	ticket: {
+		description: '未定'
+		// url: '●?uid=hp'
+	},
+	showLinkToProgramNote: false
+	// cspell: disable-next-line
+	// youtubePlaylistId: '●'
+};
