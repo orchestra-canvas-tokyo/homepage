ba128f67b75b86253501b3b7524d2f54d6b8384b
horn553 <36446753+horn553@users.noreply.github.com>
2025-10-15 19:21:55 +0900

    wip: resolveをdynamicな値に使わないように変えないといけないが……

--- src/lib/utils/resolveHref.ts
+++ src/lib/utils/resolveHref.ts
@@ -1,13 +1,13 @@
-import { resolve } from 'path';
+// import { resolve } from '$app/paths';
 
 /**
  * Resolve a possibly root-relative URL to include the configured base path.
  * External URLs are returned unchanged.
  */
 export function resolveHref(url: string): string {
-	if (url.startsWith('/')) {
-		return resolve(url);
-	}
+	// if (url.startsWith('/')) {
+	// 	return resolve(url);
+	// }
 
 	return url;
 }
