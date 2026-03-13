13f3623cb63a8daaa51e6da354dca98edb547976
Ueda-Yuto <ikimono.gnsowm.3@gmail.com>
2026-03-13 22:46:55 +0900

    feat: 第17回室内楽演奏会の情報を追加

---
+++ src/lib/concerts/chamber/chamber17.ts
@@ -0,0 +1,28 @@
+import { getConcertShortName } from '../generateContentsToDisplay';
+import type { Concert } from '../types';
+// import flyer from './images/flyers/chamber-17.png';
+
+const type = 'chamber';
+const number = 17;
+export const concert: Concert = {
+	type: type,
+	number: number,
+	slug: `${type}-${number}`,
+	title: `第${number}回${getConcertShortName(type)}演奏会`,
+	// flyer: flyer,
+	dateTime: { date: '2026-5-23', time: '昼公演' },
+	place: {
+		name: '国分寺市立いずみホール Aホール',
+		url: 'https://www.kokubunji-izumihall.jp/access/'
+	},
+	// programs: [
+	// 	{ composer: '●', title: '●' }
+	// ],
+	ticket: {
+		description: '未定'
+		// url: '●?uid=hp'
+	},
+	showLinkToProgramNote: false
+	// cspell: disable-next-line
+	// youtubePlaylistId: '●'
+};
