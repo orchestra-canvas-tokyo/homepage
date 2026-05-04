297e5a5dcc512333448cf140988391c2b91709ca
Ueda-Yuto <ikimono.gnsowm.3@gmail.com>
2026-05-04 16:42:07 +0900

    feat: 第5期の決算報告書と活動収支報告書を追加

--- src/lib/accounting/index.ts
+++ src/lib/accounting/index.ts
@@ -1,11 +1,13 @@
-import term1Pdf from './documents/第1期 決算報告書.pdf';
-import term1ActivityReportPdf from './documents/活動収支報告書_第1期_OrchestraCanvasTokyo.pdf';
-import term2Pdf from './documents/第2期 決算報告書.pdf';
-import term2ActivityReportPdf from './documents/活動収支報告書_第2期_OrchestraCanvasTokyo.pdf';
-import term3Pdf from './documents/第3期 決算報告書.pdf';
-import term3ActivityReportPdf from './documents/活動収支報告書_第3期_OrchestraCanvasTokyo.pdf';
-import term4Pdf from './documents/第4期 決算報告書.pdf';
-import term4ActivityReportPdf from './documents/活動収支報告書_第4期_OrchestraCanvasTokyo.pdf';
+import term1Pdf from './documents/第1期_決算報告書.pdf';
+import term1ActivityReportPdf from './documents/第1期_活動収支報告書.pdf';
+import term2Pdf from './documents/第2期_決算報告書.pdf';
+import term2ActivityReportPdf from './documents/第2期_活動収支報告書.pdf';
+import term3Pdf from './documents/第3期_決算報告書.pdf';
+import term3ActivityReportPdf from './documents/第3期_活動収支報告書.pdf';
+import term4Pdf from './documents/第4期_決算報告書.pdf';
+import term4ActivityReportPdf from './documents/第4期_活動収支報告書.pdf';
+import term5Pdf from './documents/第5期_決算報告書.pdf';
+import term5ActivityReportPdf from './documents/第5期_活動収支報告書.pdf';
 
 /** 決算報告についての情報をまとめた配列 */
 export const reports: {
@@ -32,5 +34,10 @@ export const reports: {
 		duration: '2024/4/1～2025/3/31',
 		pdf: term4Pdf,
 		activityReportPdf: term4ActivityReportPdf
+	},
+	{
+		duration: '2025/4/1～2026/3/31',
+		pdf: term5Pdf,
+		activityReportPdf: term5ActivityReportPdf
 	}
 ];
