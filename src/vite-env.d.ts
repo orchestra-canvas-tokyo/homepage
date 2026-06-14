a97468479ff5b04b40355bcf0f167b033e70da19
horn553 <36446753+horn553@users.noreply.github.com>
2026-06-14 17:31:59 +0900

    chore: update npm packages

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+declare module '@splidejs/svelte-splide/css/core';
+
 interface ImportMetaEnv {
 	readonly VITE_RECAPTCHA_SECRET: string;
 	readonly VITE_RESEND_API_KEY: string;
