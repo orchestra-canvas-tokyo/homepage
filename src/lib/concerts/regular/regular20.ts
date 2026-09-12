7edd05f390d03d160281152ca7f914f718a75b17
Kosuke Kitaura <36446753+horn553@users.noreply.github.com>
2026-09-12 10:09:08 +0900

    Update concert time format in regular20.ts

--- src/lib/concerts/regular/regular20.ts
+++ src/lib/concerts/regular/regular20.ts
@@ -8,7 +8,7 @@ export const concert: Concert = {
 	number: number,
 	slug: `${type}-${number}`,
 	title: `第${number}回${getConcertShortName(type)}演奏会`,
-	dateTime: { date: '2027-11-7', time: '13:00開場 14:00開演' },
+	dateTime: { date: '2027-11-7', time: '昼開演' },
 	place: {
 		name: '東京芸術劇場 コンサートホール'
 	},
