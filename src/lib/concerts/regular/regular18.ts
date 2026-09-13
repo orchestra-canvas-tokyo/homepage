83ecb97bcf5420eee8792a0e84f9644590e7fc6e
Ueda-Yuto <ikimono.gnsowm.3@gmail.com>
2026-09-13 09:28:02 +0900

    fix: 第18回定演の公演時刻とプログラム情報を更新

--- src/lib/concerts/regular/regular18.ts
+++ src/lib/concerts/regular/regular18.ts
@@ -10,7 +10,7 @@ export const concert: Concert = {
 	slug: `${type}-${number}`,
 	title: `第${number}回${getConcertShortName(type)}演奏会`,
 	// flyer: flyer,
-	dateTime: { date: '2027-1-30', time: '昼公演' },
+	dateTime: { date: '2027-1-30', time: '13:00開場 14:00開演' },
 	place: {
 		name: '武蔵野市民文化会館 大ホール',
 		url: 'https://www.musashino.or.jp/bunka/1002117/1002216.html'
@@ -18,15 +18,19 @@ export const concert: Concert = {
 	conductor: {
 		name: '岡本 陸'
 	},
-	// programs: [
-	// 	{
-	// 		composer: '●',
-	// 		title: '●'
-	// 	}
-	// ],
+	programs: [
+		{
+			composer: 'シベリウス',
+			title: '交響曲第1番 ホ短調 作品39'
+		},
+		{
+			composer: 'シベリウス',
+			title: '交響曲第2番 ニ長調 作品43'
+		}
+	],
 	ticket: {
-		description: '未定'
-		// url: '●?uid=hp'
+		description: '全席指定 1,000円',
+		url: 'https://teket.jp/1776/78089?uid=hp'
 	},
 	showLinkToProgramNote: false
 	// cspell: disable-next-line
